f1ffd0be2f77c2a01125d972f0ab9fe98577a707
Kostas Dimakis <dimakisblogging@gmail.com>
2019-02-03 14:36:58 +0000

    fix: Only show visible expenses summary in ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
 
 export const ExpensesSummary = ({total, count}) => (
     <p>
@@ -10,10 +11,14 @@ export const ExpensesSummary = ({total, count}) => (
     </p>
 );
 
-const mapStateToProps = (state) => ({
-  count: state.expenses.length,
-  total: selectExpensesTotal(state.expenses),
-});
+const mapStateToProps = (state) => {
+
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
+  return ({
+    count: visibleExpenses.length,
+    total: selectExpensesTotal(visibleExpenses),
+  });
+};
 
 export default connect(mapStateToProps)(ExpensesSummary);
 
