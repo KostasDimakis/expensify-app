1204ebad0916cb496d0c8526731645ef916d6860
Kostas Dimakis <dimakisblogging@gmail.com>
2019-02-15 23:09:58 +0000

    feat: Style Header and summary components

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,12 +3,22 @@ import {connect} from 'react-redux';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
+import {Link} from 'react-router-dom';
 
 export const ExpensesSummary = ({total, count}) => (
-    <p>
-      Viewing {count} {count === 1 ? 'expense' : 'expenses'} totalling {numeral(
-        total / 100).format('$0,0.00')}
-    </p>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">
+          Viewing <span>{count}</span> {count === 1
+            ? 'expense'
+            : 'expenses'} totalling <span>{numeral(total / 100).
+            format('$0,0.00')}</span>
+        </h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
+    </div>
 );
 
 const mapStateToProps = (state) => {
