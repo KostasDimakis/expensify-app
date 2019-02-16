c094c122032afcb608a85a08fee3d7f824a39e71
Kostas Dimakis <dimakisblogging@gmail.com>
2019-02-16 11:34:58 +0000

    chore: Update fb keys

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY ||
-      'AIzaSyBJKuF5Zi0QgmQ-sxMGkfbtORXsP7VKUVI',
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN ||
-      'expensify-app-e634b.firebaseapp.com',
-  databaseURL: process.env.FIREBASE_DATABASE_RUL ||
-      'https://expensify-app-e634b.firebaseio.com',
-  projectId: process.env.FIREBASE_PROJECT_ID || 'expensify-app-e634b',
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET ||
-      'expensify-app-e634b.appspot.com',
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '353366598979',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_RUL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
